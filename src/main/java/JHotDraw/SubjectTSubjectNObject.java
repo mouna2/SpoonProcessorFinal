cc28be45f0d300d1ed3e70581dc4df845b14373c
mouna <mouna@193.171.33.56>
2018-06-27 20:56:01 +0200

    cc

---
+++ src/main/java/JHotDraw/SubjectTSubjectNObject.java
@@ -0,0 +1,50 @@
+package JHotDraw;
+
+public class SubjectTSubjectNObject {
+	String MethodName; 
+	String RequirementID; 
+	String SubjectT; 
+	String SubjectN;
+	public String getMethodName() {
+		return MethodName;
+	}
+	public void setMethodName(String methodName) {
+		MethodName = methodName;
+	}
+	public String getRequirementID() {
+		return RequirementID;
+	}
+	public void setRequirementID(String RequirementID) {
+		this.RequirementID = RequirementID;
+	}
+	public String getSubjectT() {
+		return SubjectT;
+	}
+	public void setSubjectT(String subjectT) {
+		SubjectT = subjectT;
+	}
+	public String getSubjectN() {
+		return SubjectN;
+	}
+	public void setSubjectN(String subjectN) {
+		SubjectN = subjectN;
+	}
+	public SubjectTSubjectNObject(String methodName, String requirementID, String subjectT, String subjectN) {
+		super();
+		MethodName = methodName;
+		RequirementID = requirementID;
+		SubjectT = subjectT;
+		SubjectN = subjectN;
+	} 
+	
+	
+	public SubjectTSubjectNObject() {
+		
+	}
+	@Override
+	public String toString() {
+		return "SubjectTSubjectNObject [MethodName=" + MethodName + ", RequirementID=" + RequirementID + ", SubjectT="
+				+ SubjectT + ", SubjectN=" + SubjectN + "]";
+	} 
+	
+}
