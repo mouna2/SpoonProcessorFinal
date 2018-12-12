6d78f2bd2db88389da68de90639c946a732557e5
mouna <mouna@DESKTOP-9A91C0C>
2018-12-12 12:31:36 +0100

    cc

--- java/ALGO/AlgoFinal.java
+++ java/ALGO/AlgoFinal.java
@@ -2211,19 +2211,19 @@ public class AlgoFinal extends JFrame {
 	/************************************************************************************************************************************************/
 	/************************************************************************************************************************************************/
 	public static void main(String[] args) throws SQLException, IOException {
-//		String ProgramName = "chess";
-//		AlgoFinal frame = new AlgoFinal(
-//				ProgramName);
-//
-//		String ProgramName2 = "gantt";
-//			 frame = new AlgoFinal(ProgramName2);
-//
-//		String ProgramName3 = "itrust";
-//			 frame = new AlgoFinal(ProgramName3);
+		String ProgramName = "chess";
+		AlgoFinal frame = new AlgoFinal(
+				ProgramName);
+
+		String ProgramName2 = "gantt";
+			 frame = new AlgoFinal(ProgramName2);
+
+		String ProgramName3 = "itrust";
+			 frame = new AlgoFinal(ProgramName3);
 
 		
-		String ProgramName4 = "jhotdraw";
-		AlgoFinal	frame = new AlgoFinal(ProgramName4);
+//		String ProgramName4 = "jhotdraw";
+//		AlgoFinal	frame = new AlgoFinal(ProgramName4);
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
